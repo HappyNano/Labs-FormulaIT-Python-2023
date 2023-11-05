3882fede50cd4e5d8896b26b18e4c1c549eeee6e
Alexey <37861897+HappyNano@users.noreply.github.com>
2023-11-05 23:47:13 +0600

    Update Конвертер из CSV в JSON формат.py

--- Лабораторная работа 4/Конвертер из CSV в JSON формат.py
+++ Лабораторная работа 4/Конвертер из CSV в JSON формат.py
@@ -1,14 +1,29 @@
 # TODO импортировать необходимые молули
-
+import json
 
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
 
 def task() -> None:
-    ...  # TODO считать содержимое csv файла
+    columns_names = []  # TODO считать содержимое csv файла
+    data = []
+    with open(INPUT_FILENAME, "r") as fin:
+        is_first = True
+        for line in fin.readlines():
+            if is_first:
+                columns_names = list(map(lambda x: x.replace('\n', ''), line.split(',')))
+                is_first = False
+            else:
+                i = 0
+                json_data = {}
+                for col in line.split(','):
+                    json_data.update({ columns_names[i]: col.replace('\n', '') })
+                    i += 1
+                data.append(json_data)
 
-    ...  # TODO Сериализовать в файл с отступами равными 4
+    with open(OUTPUT_FILENAME, "w") as fout:
+        json.dump(data, fout, indent=4)
 
 
 if __name__ == '__main__':
